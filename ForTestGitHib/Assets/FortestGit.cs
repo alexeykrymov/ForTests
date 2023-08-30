2e38406c6a545824c9128c6d0da403dc6701ffac
alexeykrymov <alexeykrymov@gmail.com>
2023-08-31 02:54:51 +0400

    For test GIT
    `For test GIT

---
+++ ForTestGitHib/Assets/FortestGit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FortestGit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // for test git hub
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
