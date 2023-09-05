4d251dd6743199244f80f1f318052968536cb6b6
alexeykrymov <alexeykrymov@gmail.com>
2023-09-05 21:27:36 +0400

    gg

--- ForTestGitHib/Assets/NewBehaviourScript12.cs
+++ ForTestGitHib/Assets/NewBehaviourScript12.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JustForDelete : MonoBehaviour
+public class NewBehaviourScript12 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
